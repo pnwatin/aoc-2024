a43178753694e2403ea13c344d479460ef120948
Pierre-Nicolas Watin-Augouard <pnwa@icloud.com>
2024-12-01 16:53:41 +0100

    chore: refactor pretty printing into AdventDay trait

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::fmt::Display;
+use std::{fmt::Display, time::Instant};
 
 mod day01;
 
@@ -9,6 +9,34 @@ pub type Result<T> = std::result::Result<T, Error>;
 
 pub trait AdventDay {
     fn day_number(&self) -> u32;
+
     fn part_one(&self) -> Result<Box<dyn Display>>;
+
     fn part_two(&self) -> Result<Box<dyn Display>>;
+
+    fn pretty(&self) {
+        println!("\nDay {}", self.day_number());
+
+        let part_one_start = Instant::now();
+        let part_one_result = match self.part_one() {
+            Ok(res) => res.to_string(),
+            Err(e) => format!("Error: {}", e),
+        };
+        println!(
+            "- ⭐    result : {} (took {:?})",
+            part_one_result,
+            part_one_start.elapsed(),
+        );
+
+        let part_two_start = Instant::now();
+        let part_two_result = match self.part_two() {
+            Ok(res) => res.to_string(),
+            Err(e) => format!("Error: {}", e),
+        };
+        println!(
+            "- ⭐⭐  result : {} (took {:?})",
+            part_two_result,
+            part_two_start.elapsed(),
+        );
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,11 @@
-use std::time::Instant;
-
 use aoc::{AdventDay, Day01};
-
-fn pretty_print_day(day: &dyn AdventDay) {
-    println!("\nDay {}", day.day_number());
-
-    let part_one_start = Instant::now();
-    let part_one_result = match day.part_one() {
-        Ok(res) => res.to_string(),
-        Err(e) => format!("Error: {}", e),
-    };
-    println!(
-        "- ⭐    result : {} (took {:?})",
-        part_one_result,
-        part_one_start.elapsed(),
-    );
-
-    let part_two_start = Instant::now();
-    let part_two_result = match day.part_two() {
-        Ok(res) => res.to_string(),
-        Err(e) => format!("Error: {}", e),
-    };
-    println!(
-        "- ⭐⭐  result : {} (took {:?})",
-        part_two_result,
-        part_two_start.elapsed(),
-    );
-}
 
 fn main() {
     println!("AoC 2024 [pwnatin]");
 
-    let days: Vec<&dyn AdventDay> = vec![&Day01];
+    let days = vec![Day01];
 
     for day in days {
-        pretty_print_day(day)
+        day.pretty();
     }
 }
